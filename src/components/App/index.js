201619866d61db367d5da5c7811ea8873527b4ad
Anderson de Souza <anderson.souza.ext@infracommerce.com.br>
2022-02-04 18:36:18 -0300

    Ajustando rotas

---
+++ src/components/App/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import RoutesApp from "../../routes";
+
+export default function App() {
+  return (
+    <div className="app">
+      <RoutesApp />
+    </div>
+  );
+}
